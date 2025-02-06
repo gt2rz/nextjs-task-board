146f5407603c0ad08085266afe72b72048cd80e3
Miguel Gutierrez <gt2rz.dev@gmail.com>
2025-02-05 22:51:19 -0500

    Improve comment display in TaskCardComments for better layout and styling

--- components/projects/TaskCardComments.tsx
+++ components/projects/TaskCardComments.tsx
@@ -2,21 +2,19 @@ import { Comment } from "@/types";
 
 export default function TaskCardComments({ comments }: { comments: Comment[] }) {
     return (
-        <div className="flex items-center my-4">
-            {comments.length > 0 && (
-                <div className="flex items-center">
-                    {
-                        comments.length > 0 && comments.map((comment, index) => (
-                            <div key={`comment-${comment.id}`}>
-                                <img src={comment.avatar} alt="avatar" className={`w-6 h-6 rounded-full border ${index > 0 ? '-ml-2' : ''}`} />
-                            </div>
-                        ))
-                    }
+        <>
+            {comments?.length > 0 && (
+                <div className="flex items-center my-4 space-x-3">
+                    <div className="flex items-center -space-x-2">
+                        {comments.length > 0 && comments.slice(0, 3).map((comment) => (
+                                <img key={`comment-${comment.id}`} src={comment.avatar} alt="avatar" className="inline-block size-8 rounded-full ring-1 ring-gray-300 bg-white" />
+                        ))}
+                    </div>
                     <span className="text-gray-500 text-sm ml-1 font-semibold">
                         {comments.length} {comments.length > 1 ? 'comments' : 'comment'}
                     </span>
                 </div>
             )}
-        </div>
+        </>
     );
 }
